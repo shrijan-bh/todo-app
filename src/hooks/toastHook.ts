2ce7d8fbbf679f619f2b1abfeaedb72f66d9a6bf
Shrijan <shrijanb85@gmail.com>
2023-04-11 19:37:24 +0545

    toast-added

---
+++ src/hooks/toastHook.ts
@@ -0,0 +1,33 @@
+import { toast } from "react-toastify";
+
+export const toastHook = ({
+  message,
+  type,
+}: {
+  message: string;
+  type: string;
+}) => {
+  if (type === "success") {
+    toast.success(message, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  } else {
+    toast.warning(message, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
+};
